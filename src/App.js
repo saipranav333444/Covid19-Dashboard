2526c5c03990e7a98ed2e07766671b699b4e30b4
saipranav333444 <109069619+saipranav333444@users.noreply.github.com>
2024-10-21 17:47:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NotFoundPage from './components/NotFoundPage'
 
 import StateWiseDataPage from './components/StateWiseDataPage'
 
-import VaccinationPage from './components/VaccinationPage'
+
 
 import About from './components/About'
 
@@ -17,7 +17,7 @@ const App = () => (
     <Route exact path="/" component={Home} />
     <Route exact path="/state/:stateCode" component={StateWiseDataPage} />
     <Route exact path="/about" component={About} />
-    <Route exact path="/vaccination" component={VaccinationPage} />
+  
     <Route component={NotFoundPage} />
   </Switch>
 )
